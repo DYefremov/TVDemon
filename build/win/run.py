364b0ab2edaab5cd21257f845fbc392f4bdc2fb3
DYefremov <dmitry.v.yefremov@gmail.com>
2023-09-14 17:00:45 +0300

    minor adjustment

--- build/win/run.py
+++ build/win/run.py
@@ -5,15 +5,12 @@ if __name__ == "__main__":
     if getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS"):
         import cairo
         import os
-        
         import ssl
         from multiprocessing import freeze_support
+        from usr.lib.tvdemon.app.main import run
 
         os.environ["PYTHONUTF8"] = "1"
         ssl._create_default_https_context = ssl._create_unverified_context
 
         freeze_support()
-
-        from usr.lib.tvdemon.app.main import run
-
         run()
