29171eff9d6b2de5a0448328eac58685e4f86480
DYefremov <dmitry.v.yefremov@gmail.com>
2023-09-22 09:48:16 +0300

    replaced some icons

--- usr/lib/tvdemon/app/main.py
+++ usr/lib/tvdemon/app/main.py
@@ -958,7 +958,7 @@ class Application(Gtk.Application):
             button.set_relief(Gtk.ReliefStyle.NONE)
             button.connect("clicked", self.on_edit_button_clicked, provider)
             image = Gtk.Image()
-            image.set_from_icon_name("xapp-edit-symbolic", Gtk.IconSize.BUTTON)
+            image.set_from_icon_name("document-edit-symbolic", Gtk.IconSize.BUTTON)
             button.set_tooltip_text(_("Edit"))
             button.add(image)
             box.pack_start(button, False, False, 0)
